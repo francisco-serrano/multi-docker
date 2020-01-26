ec3411bb2468ae8a2324ddaff97223f2168fb640
Francisco Serrano <francisco.serrano372@gmail.com>
2020-01-26 16:50:41 -0300

    initial commit

---
+++ client/src/App.test.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+// the ideal way is to mock out the backend requests
+test('renders learn react link', () => {});
